7067bcfa227008a43f7fffd29212278cca9a11c4
IBars <aybarscabioglu@gmail.com>
2026-08-15 13:42:46 +0300

    Reset Button Added

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,10 +5,16 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        // 1. Level'de olduğumuzu PlayerPrefs ile hafızaya kaydediyoruz (ileride seviye yazısını güncellemek için işimize yarayacak)
         PlayerPrefs.SetInt("CurrentLevel", 1);
-        
-        // Oyun sahnesini yüklüyoruz. ("GameScene" yazan yeri kendi oyun sahnenin adıyla değiştir)
         SceneManager.LoadScene("GameScene"); 
+    }
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+
+        // Sıfırlama sonrası anında görmek için Menu sahnesini tekrar yükle
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
